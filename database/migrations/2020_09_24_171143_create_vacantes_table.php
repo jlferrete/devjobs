0bc5e5c928cf01955d9f5d8847b2bf643dd39974
jlferrete <joseluisferrete@gmail.com>
2020-09-24 19:19:02 +0200

    Creating the table of vacancies and categories

--- database/migrations/2020_09_24_171143_create_vacantes_table.php
+++ database/migrations/2020_09_24_171143_create_vacantes_table.php
@@ -15,6 +15,7 @@ class CreateVacantesTable extends Migration
     {
         Schema::create('vacantes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('categoria_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
